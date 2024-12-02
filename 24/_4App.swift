92d83e58c647174c3678d1643837f7b4edd9f77e
JonathanVarghese <jvarghese@bsd220.org>
2024-12-02 09:41:12 -0600

    Initial Commit

---
+++ 24/_4App.swift
@@ -0,0 +1,17 @@
+//
+//  _4App.swift
+//  24
+//
+//  Created by Jonathan V on 12/2/24.
+//
+
+import SwiftUI
+
+@main
+struct _4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 24Tests/_4Tests.swift
@@ -0,0 +1,16 @@
+//
+//  _4Tests.swift
+//  24Tests
+//
+//  Created by Jonathan V on 12/2/24.
+//
+
+import Testing
+
+struct _4Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
